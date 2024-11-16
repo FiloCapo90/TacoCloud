85da87c37f8a4d8fb5331e9b2bd0044add3f58a2
giorg <giorg@DESKTOP-DCR8L1V>
2024-11-16 19:09:13 +0100

    Second Commit, chapter 2.3.2, page 48

--- src/main/java/com/example/tacos/web/OrderController.java
+++ src/main/java/com/example/tacos/web/OrderController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -16,6 +17,12 @@ public class OrderController {
     public String OrderForm(Model model){
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+    
+    @PostMapping
+    public String processOrder(Order order){
+        log.info("Order submitted: "+ order);
+        return "redirect:/";
     }
     
 }
